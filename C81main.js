c929544039a2de83745b48539300e9ab6627cc28
viviktaiyer <82214501+viviktaiyer@users.noreply.github.com>
2021-05-03 17:59:45 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,39 @@
+canvas=document.getElementById("canva");
+ctx=canvas.getContext("2d");
+color="red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,201,44,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e){
+color=document.getElementById("color").value;
+console.log(color);
+mouse_x=e.clientX-canvas.offsetLeft;
+mouse_y=e.clientY-canvas.offsetTop;
+circle(mouse_x,mouse_y);
+
+}
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mouse_x,mouse_y,44,0,2*Math.PI);
+    ctx.stroke();
+
+}
+function clearArea(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+
+
+
+
+
+
+
+
+
